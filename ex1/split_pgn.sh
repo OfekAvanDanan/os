e32914d951b9a6ec1fc1de07284afcf9ed454012
OfekAvanDanan <4ofek4dj@gmail.com>
2024-11-16 15:36:48 +0200

    split to functions

--- ex1/split_pgn.sh
+++ ex1/split_pgn.sh
@@ -1,21 +1,55 @@
 #!/bin/bash
 
-# Argument Validation 
-if [ "$#" -ne 2 ]; then
-  echo "Usage: $0 <source_pgn_file> <destination_directory>"
-  exit 1
-fi
-
-
-# File and Directory Existence Check
-file="$1"
-dir="$2"
-
-if [ ! -f "$file" ]; then
-    echo "Error: File '$file' does not exist."
+# FUNCTIONS - 
+# Argument Validation - makes sure thate there are 2 arguments
+argumentValidation() {
+    echo "Usage: $0 <source_pgn_file> <destination_directory>"
     exit 1
-elif  [ ! -d "$dir" ]; then
-    mkdir -p "$dir"
-    echo "Created directory '$dir'."
+}
+
+fileExistence(){
+    local file="$1"
+
+    if [ ! -f "$file" ]; then
+        echo "Error: File '$file' does not exist."
+        exit 1
+    fi
+}
+
+directoryExistence(){
+    local dir="$1"
+
+    if  [ ! -d "$dir" ]; then
+        mkdir -p "$dir"
+        echo "Created directory '$dir'."
+    fi
+}
+
+splitPGNFile(){
+    local file="$1"
+    local dir="$2"
+
+    local gameNum=1
+    local currGameFile
+
+    echo "'$file' '$dir' '$gameNum'."
+}
+
+# Main Function flow
+#======================
+if [ "$#" -ne 2 ]; then
+    argumentValidation
 fi
 
+input_file="$1"
+input_dir="$2"
+
+fileExistence "$input_file"
+directoryExistence "$input_dir"
+
+splitPGNFile "$input_file" "$input_dir"
+
+
+
+
+# Sp
