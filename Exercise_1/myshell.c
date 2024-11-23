ab3f706e35c4061bb4178f941ccd2113eb64e091
OfekAvanDanan <4ofek4dj@gmail.com>
2024-11-23 12:41:52 +0200

    history

--- Exercise_1/myshell.c
+++ Exercise_1/myshell.c
@@ -1,10 +1,6 @@
-#include <errno.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#include <sys/types.h>
-#include <sys/wait.h>
-#include <unistd.h>
 
 // Global variables
 char *history[1000];
