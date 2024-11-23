5a7349b97ac7863edf93d7984a49401730953efe
OfekAvanDanan <4ofek4dj@gmail.com>
2024-11-23 15:18:52 +0200

    added args for commands and cd

--- Exercise_1/myshell.c
+++ Exercise_1/myshell.c
@@ -22,11 +22,63 @@ void exitShell() {
   exit(0);
 }
 
+void changeDirectory(char *path) {
+  if (chdir(path) != 0) {
+    perror("cd failed");
+  }
+}
+
 void execute(char *input) {
-  if (strcmp(input, "history") == 0) {
+  // Split the input into args
+  char *args[100];
+  int argc = 0; // Counter for arguments
+  char *currChar = input;
+
+  while (*currChar != '\0') {
+    // Skip spaces
+    while (*currChar == ' ') {
+      currChar++;
+    }
+
+    // Check if we reached the end of the string
+    if (*currChar == '\0') {
+      break;
+    }
+
+    // Save the start of the argument
+    args[argc++] = currChar;
+
+    // Move to the end of the current argument
+    while (*currChar != '\0' && *currChar != ' ') {
+      currChar++;
+    }
+
+    // If we found a space, terminate the argument and move on
+    if (*currChar == ' ') {
+      *currChar = '\0';
+      currChar++;
+    }
+  }
+
+  // Null-terminate the args array for execvp
+  args[argc] = NULL;
+
+  // Execute the command
+  if (argc == 0) {
+    // No command entered, just return
+    return;
+  }
+
+  if (strcmp(args[0], "history") == 0) {
     printHistory();
-  } else if (strcmp(input, "exit") == 0) {
+  } else if (strcmp(args[0], "exit") == 0) {
     exitShell();
+  } else if (strcmp(args[0], "cd") == 0) {
+    if (argc < 2) {
+      fprintf(stderr, "cd: missing argument\n");
+    } else {
+      changeDirectory(args[1]);
+    }
   }
 }
 
