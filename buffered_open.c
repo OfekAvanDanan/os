391d5de46dd8bdac6d34c8330bfe1ca6ad96d4cc
Azriel Erenkrzntz <clickazr@gmail.com>
2024-12-16 09:20:44 +0200

    Update buffered_open.c -update for linux

--- buffered_open.c
+++ buffered_open.c
@@ -1,34 +1,70 @@
+// Azriel Erenkrantz - 315336719 | Ofek Avan Danan - 211824727
 #include "buffered_open.h"
 #include <stdlib.h>
 #include<stdio.h>
 #include <string.h>
 #include <signal.h>
+#include <stdarg.h>
 
 buffered_file_t *globalBuffer; //for sigint signal handle
 buffered_file_t *buffered_open(const char *pathname, int flags, ...) {
-    //allocate memory for the struct
+    int mode = 0644; // Default mode for creating files if O_CREAT is used
+    va_list args;
+
+    // Allocate memory for the buffered file structure
     buffered_file_t *bf = (buffered_file_t*)calloc(1, sizeof(buffered_file_t));
-    //if O_PREAPPEND is parts of the flags - preappend =1;
-    if (flags&O_PREAPPEND) {
+    if (bf == NULL) {
+        perror("Failed to allocate memory for buffered file");
+        return NULL;
+    }
+
+    // Remove O_PREAPPEND from flags (it should not interfere with open)
+    if (flags & O_PREAPPEND) {
         bf->preappend = 1;
     }
-    //set fd
-    bf->flags&~O_PREAPPEND; //remove O_PREAPPEND from flags
-    if (bf->flags&O_CREAT) {
-        int mode = 0644;
-        bf->fd = open(pathname,flags,mode);
-    } else { bf->fd = open(pathname,flags); }
-    //allocate memory for the buffers
-    bf->read_buffer = (char*) calloc(1,BUFFER_SIZE );
-    bf->write_buffer = (char*) calloc(1,BUFFER_SIZE );
+    flags &= ~O_PREAPPEND;
+
+    // Store the flags
+    bf->flags = flags;
+
+    // Check if O_CREAT is part of the flags
+    if (flags & O_CREAT) {
+        // If O_CREAT is set, we need to retrieve the mode 
+        va_start(args, flags);
+        mode = va_arg(args, int);
+        va_end(args);
+    }
+
+    // Open the file with the flags and mode (if applicable)
+    bf->fd = open(pathname, flags, mode);
+    if (bf->fd == -1) {
+        perror("Failed to open file");
+        free(bf);
+        return NULL;
+    }
+
+    // Allocate memory for read and write buffers
+    bf->read_buffer = (char*)malloc(BUFFER_SIZE);
+    bf->write_buffer = (char*)malloc(BUFFER_SIZE);
+    if (bf->read_buffer == NULL || bf->write_buffer == NULL) {
+        perror("failed to allocate buffers");
+        close(bf->fd);
+        free(bf->read_buffer);
+        free(bf->write_buffer);
+        free(bf);
+        return NULL;
+    }
+
+    // Set buffer sizes and positions
     bf->read_buffer_size = 0;
     bf->write_buffer_size = 0;
     bf->read_buffer_pos = 0;
     bf->write_buffer_pos = 0;
-    globalBuffer = bf; //set the global buffer to point on the current exsist buffer
+
+     globalBuffer = bf; //set the global buffer to point on the current exsist buffer
     //close the program properly
     if (atexit(atexit_care) != 0) {
-        perror("Failed to register atexit");
+        perror("failed to register atexit");
         return NULL;
     }
     if (signal(SIGINT,sigint_care) == SIG_ERR) {
@@ -36,6 +72,7 @@ buffered_file_t *buffered_open(const char *pathname, int flags, ...) {
         return NULL;
     }
 
+    // Return the buffered file structure
     return bf;
 }
 
@@ -93,20 +130,22 @@ ssize_t buffered_read(buffered_file_t *bf, void *buf, size_t count) {
     //start reading
     int  actuallyRead = 0;
     int aviliableToRead = 0;
+
     while(actuallyRead < count) {
         if (bf->read_buffer_pos >= bf->read_buffer_size || bf->read_buffer_size == 0) {
-            int readFromFile = read(bf->fd,bf->read_buffer, count);
+            int readFromFile = read(bf->fd,bf->read_buffer, BUFFER_SIZE);
             if (readFromFile < 0) {
                 return -1;  // Error
             }
             if(readFromFile == 0) { //end od file
                 break;
             }
-            bf->read_buffer_size += readFromFile;
+            bf->read_buffer_size = readFromFile;
+            bf->read_buffer_pos = 0;
         }
         aviliableToRead = bf->read_buffer_size - bf->read_buffer_pos;
         int toCopy =(count - actuallyRead < aviliableToRead)?count - actuallyRead:aviliableToRead;
-        memcpy(buf + actuallyRead, bf->read_buffer ,toCopy);
+        memcpy(buf + actuallyRead, bf->read_buffer + bf->read_buffer_pos ,toCopy);
         bf->read_buffer_pos += toCopy;
         actuallyRead += toCopy;
     }
