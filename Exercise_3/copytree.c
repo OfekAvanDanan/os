b62af15b652a3aab75f04f9a7ffaffc7fbb6b1de
OfekAvanDanan <4ofek4dj@gmail.com>
2024-12-19 18:15:15 +0200

    fix errors

--- Exercise_3/copytree.c
+++ Exercise_3/copytree.c
@@ -3,121 +3,103 @@
 #include <errno.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 #include <sys/stat.h>
 #include <unistd.h>
 
-#define PATH_MAX 1024
 #define BUFFER_SIZE 1024
 
-// Function to copy a file
+void create_directories(const char *path) {
+  char tmp[PATH_MAX];
+  snprintf(tmp, sizeof(tmp), "%s", path);
+  char *p = tmp;
+
+  while (*p) {
+    if (*p == '/') {
+      *p = '\0';
+      mkdir(tmp, 0755);
+      *p = '/';
+    }
+    p++;
+  }
+  mkdir(tmp, 0755);
+}
+
 void copy_file(const char *src, const char *dest, int copy_symlinks, int copy_permissions) {
   struct stat fileStat;
-
-  // Get file stats
-  if (stat(src, &fileStat) == -1) {
+  if (lstat(src, &fileStat) == -1) {
     perror("COMMAND failed");
     exit(EXIT_FAILURE);
   }
 
   char srcTarget[PATH_MAX];
-
-  // Check if the source is a symbolic link
   if (S_ISLNK(fileStat.st_mode)) {
-    int len = readlink(src, srcTarget, sizeof(srcTarget) - 1);
-
+    ssize_t len = readlink(src, srcTarget, sizeof(srcTarget) - 1);
     if (len == -1) {
       perror("COMMAND failed");
       exit(EXIT_FAILURE);
     }
-
-    // Null-terminate the target path
     srcTarget[len] = '\0';
-  } else if (S_ISREG(fileStat.st_mode)) {
-    // If the file is regular, copy the file path
+  } else {
     strcpy(srcTarget, src);
   }
 
-  // Start copying the file
   if (copy_symlinks) {
     if (symlink(srcTarget, dest) == -1) {
       perror("COMMAND failed");
       exit(EXIT_FAILURE);
     }
-  } else {
-    int src_fd, dest_fd;
-    char buffer[BUFFER_SIZE];
-    ssize_t bytes_read, bytes_written;
+    return;
+  }
 
-    // Open source file
-    src_fd = open(srcTarget, O_RDONLY);
-    if (src_fd == -1) {
-      perror("COMMAND failed");
-      exit(EXIT_FAILURE);
-    }
+  int src_fd = open(srcTarget, O_RDONLY);
+  if (src_fd == -1) {
+    perror("COMMAND failed");
+    exit(EXIT_FAILURE);
+  }
 
-    // Ensure destination directories exist
-    char destDir[PATH_MAX];
-    strncpy(destDir, dest, sizeof(destDir));
-    char *lastSlash = strrchr(destDir, '/');
-    if (lastSlash) {
-      *lastSlash = '\0';
-      create_directories(destDir);
-    }
+  create_directories(dest);
 
-    // Open destination file
-    dest_fd = open(dest, O_WRONLY | O_CREAT | O_TRUNC, 0644);
-    if (dest_fd == -1) {
+  int dest_fd = open(dest, O_WRONLY | O_CREAT | O_TRUNC, 0644);
+  if (dest_fd == -1) {
+    perror("COMMAND failed");
+    close(src_fd);
+    exit(EXIT_FAILURE);
+  }
+
+  char buffer[BUFFER_SIZE];
+  ssize_t bytes_read, bytes_written;
+  while ((bytes_read = read(src_fd, buffer, BUFFER_SIZE)) > 0) {
+    bytes_written = write(dest_fd, buffer, bytes_read);
+    if (bytes_written != bytes_read) {
       perror("COMMAND failed");
       close(src_fd);
+      close(dest_fd);
       exit(EXIT_FAILURE);
     }
+  }
 
-    // Copy file content
-    while ((bytes_read = read(src_fd, buffer, BUFFER_SIZE)) > 0) {
-      bytes_written = write(dest_fd, buffer, bytes_read);
-      if (bytes_written != bytes_read) {
-        perror("COMMAND failed");
-        close(src_fd);
-        close(dest_fd);
-        exit(EXIT_FAILURE);
-      }
-    }
-
-    if (bytes_read == -1) {
+  if (copy_permissions) {
+    if (chmod(dest, fileStat.st_mode) == -1) {
       perror("COMMAND failed");
+      close(src_fd);
+      close(dest_fd);
+      exit(EXIT_FAILURE);
     }
-
-    // Apply permissions if copy_permissions is enabled
-    if (copy_permissions) {
-      if (chmod(dest, fileStat.st_mode) == -1) {
-        perror("COMMAND failed");
-        close(src_fd);
-        close(dest_fd);
-        exit(EXIT_FAILURE);
-      }
-    }
-
-    // Close files
-    close(src_fd);
-    close(dest_fd);
   }
-}
 
-void create_directory(const char *path) {
-  if (mkdir(path, 0755) == -1 && errno != EEXIST) {
-    perror("COMMAND failed");
-  }
+  close(src_fd);
+  close(dest_fd);
 }
 
 void copy_directory(const char *src, const char *dest, int copy_symlinks, int copy_permissions) {
-  struct DIR *srcDir;
-  // Check the source directory
-  if ((srcDir = opendir(src)) == NULL) {
+  DIR *srcDir = opendir(src);
+  if (srcDir == NULL) {
     perror("COMMAND failed");
+    exit(EXIT_FAILURE);
   }
 
-  // Create destination directory
-  create_directory(dest);
+  create_directories(dest);
 
   struct dirent *entry;
   while ((entry = readdir(srcDir)) != NULL) {
@@ -127,6 +109,8 @@ void copy_directory(const char *src, const char *dest, int copy_symlinks, int co
 
     char src_path[PATH_MAX];
     char dest_path[PATH_MAX];
+    snprintf(src_path, sizeof(src_path), "%s/%s", src, entry->d_name);
+    snprintf(dest_path, sizeof(dest_path), "%s/%s", dest, entry->d_name);
 
     struct stat stat_buf;
     if (lstat(src_path, &stat_buf) == -1) {
@@ -135,10 +119,8 @@ void copy_directory(const char *src, const char *dest, int copy_symlinks, int co
     }
 
     if (S_ISDIR(stat_buf.st_mode)) {
-      // Recursively copy subdirectory
       copy_directory(src_path, dest_path, copy_symlinks, copy_permissions);
     } else {
-      // Copy file
       copy_file(src_path, dest_path, copy_symlinks, copy_permissions);
     }
   }
--- Exercise_3/part3.c
+++ Exercise_3/part3.c
@@ -1,21 +1,19 @@
 #include "copytree.h"
-#include <bits/getopt_core.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
 
 void print_usage(const char *prog_name) {
-  fprintf(stderr, "Usage: %s [-l] [-p] <source_directory> <destination_directory>\n", prog_name);
-  fprintf(stderr, "  -l: Copy symbolic links as links\n");
-  fprintf(stderr, "  -p: Copy file permissions\n");
+  fprintf(stderr, "Usage: %s [-l] [-p] <source_directory> <destination_directory>", prog_name);
+  fprintf(stderr, "  -l: Copy symbolic links as links");
+  fprintf(stderr, "  -p: Copy file permissions");
 }
 
 int main(int argc, char *argv[]) {
   int opt;
-  int copy_symlinks = 0;    // Default: Do not copy symlinks as links
-  int copy_permissions = 0; // Default: Do not copy permissions
+  int copy_symlinks = 0;
+  int copy_permissions = 0;
 
-  // Parse command-line options
   while ((opt = getopt(argc, argv, "lp")) != -1) {
     switch (opt) {
     case 'l':
@@ -30,22 +28,15 @@ int main(int argc, char *argv[]) {
     }
   }
 
-  // Ensure the correct number of arguments (source and destination directories)
   if (optind + 2 != argc) {
     print_usage(argv[0]);
     return EXIT_FAILURE;
   }
 
-  const char *src_dir = argv[optind];      // Source directory
-  const char *dest_dir = argv[optind + 1]; // Destination directory
-
-  // Perform the directory copy
-  printf("Copying from '%s' to '%s'...\n", src_dir, dest_dir);
-  printf("Options: Copy symbolic links: %s, Copy permissions: %s\n", copy_symlinks ? "ON" : "OFF",
-         copy_permissions ? "ON" : "OFF");
+  const char *src_dir = argv[optind];
+  const char *dest_dir = argv[optind + 1];
 
   copy_directory(src_dir, dest_dir, copy_symlinks, copy_permissions);
 
-  printf("Directory copied successfully.\n");
-  return EXIT_SUCCESS;
+  return 0;
 }
