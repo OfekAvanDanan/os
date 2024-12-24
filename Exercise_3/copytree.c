d1dd6556afa7acd03839d7826eff0a5b0f9b39c2
OfekAvanDanan <4ofek4dj@gmail.com>
2024-12-24 19:45:53 +0200

    update

--- Exercise_3/copytree.c
+++ Exercise_3/copytree.c
@@ -27,79 +27,87 @@ void create_directories(const char *path) {
 
 void copy_file(const char *src, const char *dest, int copy_symlinks, int copy_permissions) {
   struct stat fileStat;
+
+  // Check the source path
   if (lstat(src, &fileStat) == -1) {
     perror("COMMAND failed");
     exit(EXIT_FAILURE);
   }
 
-  char srcTarget[PATH_MAX];
+  // Handle symbolic links
   if (S_ISLNK(fileStat.st_mode)) {
-    ssize_t len = readlink(src, srcTarget, sizeof(srcTarget) - 1);
-    if (len == -1) {
-      perror("COMMAND failed");
-      exit(EXIT_FAILURE);
-    }
-    srcTarget[len] = '\0';
-  } else {
-    strcpy(srcTarget, src);
-  }
-
-  if (copy_symlinks) {
-    if (symlink(srcTarget, dest) == -1) {
-      perror("COMMAND failed");
-      exit(EXIT_FAILURE);
+    if (copy_symlinks) {
+      char srcTarget[PATH_MAX];
+      ssize_t len = readlink(src, srcTarget, sizeof(srcTarget) - 1);
+      if (len == -1) {
+        perror("COMMAND failed");
+        exit(EXIT_FAILURE);
+      }
+      srcTarget[len] = '\0';
+      if (symlink(srcTarget, dest) == -1) {
+        perror("COMMAND failed");
+        exit(EXIT_FAILURE);
+      }
     }
     return;
   }
 
-  int srcFd = open(srcTarget, O_RDONLY);
-  if (srcFd == -1) {
-    perror("COMMAND failed");
-    exit(EXIT_FAILURE);
-  }
+  // Handle regular files
+  if (S_ISREG(fileStat.st_mode)) {
+    int srcFd = open(src, O_RDONLY);
+    if (srcFd == -1) {
+      perror("COMMAND failed");
+      exit(EXIT_FAILURE);
+    }
 
-  create_directories(dest);
+    int destFd = open(dest, O_WRONLY | O_CREAT | O_TRUNC, fileStat.st_mode & 0777);
+    if (destFd == -1) {
+      perror("COMMAND failed");
+      close(srcFd);
+      exit(EXIT_FAILURE);
+    }
 
-  int destFd = open(dest, O_WRONLY | O_CREAT | O_TRUNC, 0644);
-  if (destFd == -1) {
-    perror("COMMAND failed");
+    char buffer[BUFFER_SIZE];
+    ssize_t bytesRead, bytesWritten;
+    while ((bytesRead = read(srcFd, buffer, BUFFER_SIZE)) > 0) {
+      bytesWritten = write(destFd, buffer, bytesRead);
+      if (bytesWritten != bytesRead) {
+        perror("COMMAND failed");
+        close(srcFd);
+        close(destFd);
+        exit(EXIT_FAILURE);
+      }
+    }
+
+    if (bytesRead == -1) {
+      perror("COMMAND failed");
+    }
+
     close(srcFd);
-    exit(EXIT_FAILURE);
+    close(destFd);
   }
-
-  char buffer[BUFFER_SIZE];
-  ssize_t bytes_read, bytes_written;
-  while ((bytes_read = read(srcFd, buffer, BUFFER_SIZE)) > 0) {
-    bytes_written = write(destFd, buffer, bytes_read);
-    if (bytes_written != bytes_read) {
-      perror("COMMAND failed");
-      close(srcFd);
-      close(destFd);
-      exit(EXIT_FAILURE);
-    }
-  }
-
-  if (copy_permissions) {
-    if (chmod(dest, fileStat.st_mode) == -1) {
-      perror("COMMAND failed");
-      close(srcFd);
-      close(destFd);
-      exit(EXIT_FAILURE);
-    }
-  }
-
-  close(srcFd);
-  close(destFd);
 }
 
 void copy_directory(const char *src, const char *dest, int copy_symlinks, int copy_permissions) {
-  DIR *srcDir = opendir(src);
-  if (srcDir == NULL) {
+  struct stat statBuf;
+
+  // Check if the source is a directory
+  if (stat(src, &statBuf) == -1 || !S_ISDIR(statBuf.st_mode)) {
+    fprintf(stderr, "Source is not a directory: %s\n", src);
+    exit(EXIT_FAILURE);
+  }
+
+  // Create the target directory
+  if (mkdir(dest, 0755) == -1 && errno != EEXIST) {
     perror("COMMAND failed");
     exit(EXIT_FAILURE);
   }
 
-  create_directories(dest);
+  DIR *srcDir = opendir(src);
+  if (!srcDir) {
+    perror("COMMAND failed");
+    exit(EXIT_FAILURE);
+  }
 
   struct dirent *entry;
   while ((entry = readdir(srcDir)) != NULL) {
@@ -112,15 +120,16 @@ void copy_directory(const char *src, const char *dest, int copy_symlinks, int co
     snprintf(srcPath, sizeof(srcPath), "%s/%s", src, entry->d_name);
     snprintf(destPath, sizeof(destPath), "%s/%s", dest, entry->d_name);
 
-    struct stat stat_buf;
-    if (lstat(srcPath, &stat_buf) == -1) {
-      perror("lstat failed");
+    if (lstat(srcPath, &statBuf) == -1) {
+      perror("COMMAND failed");
       continue;
     }
 
-    if (S_ISDIR(stat_buf.st_mode)) {
+    if (S_ISDIR(statBuf.st_mode)) {
+      // Recursive call for directories
       copy_directory(srcPath, destPath, copy_symlinks, copy_permissions);
     } else {
+      // Call to copy files
       copy_file(srcPath, destPath, copy_symlinks, copy_permissions);
     }
   }
