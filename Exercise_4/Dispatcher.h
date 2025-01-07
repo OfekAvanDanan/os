75feabd2a4fad1a892a8c467833d4d3c15443521
azriel <clickazr@gmail.com>
2025-01-07 21:47:31 +0200

    first dispatcher

---
+++ Exercise_4/Dispatcher.h
@@ -0,0 +1,18 @@
+class Dispatcher
+{
+private:
+    queue *S_dispatcher_queue;
+    queue *N_dispatcher_queue;
+    queue *W_dispatcher_queue;
+    queue *ready;
+    //pointer to vector of pointers to the producers queue
+    std::queue<std::queue<std::string>*>*
+
+
+public:
+Dispatcher:Dispatcher(queue *S_dispatcher_queue, queue *N_dispatcher_queue, queue *W_dispatcher_queue, std::queue<std::queue<std::string>*>*)
+    for (int i=0; i<numOfProducers; ++i) {
+        if (queue[])
+    }
+};
+
