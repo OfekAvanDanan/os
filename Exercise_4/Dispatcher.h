ba646ae62f1b277728da9d4e21c432a4a6de2897
azriel <clickazr@gmail.com>
2025-01-12 00:27:06 +0200

    constractur

--- Exercise_4/Dispatcher.h
+++ Exercise_4/Dispatcher.h
@@ -1,18 +1,21 @@
+#pragma once
+#include "Producer.h"
 class Dispatcher
 {
 private:
-    queue *S_dispatcher_queue;
-    queue *N_dispatcher_queue;
-    queue *W_dispatcher_queue;
-    queue *ready;
+    BoundedQueue *S_dispatcher_queue;
+    BoundedQueue *N_dispatcher_queue;
+    BoundedQueue *W_dispatcher_queue;
+    BoundedQueue *ready;
+    int numOfProducers;
     //pointer to vector of pointers to the producers queue
-    std::queue<std::queue<std::string>*>*
+    std::vector<Producer*>* producerVector;
 
 
-public:
-Dispatcher:Dispatcher(queue *S_dispatcher_queue, queue *N_dispatcher_queue, queue *W_dispatcher_queue, std::queue<std::queue<std::string>*>*)
-    for (int i=0; i<numOfProducers; ++i) {
-        if (queue[])
-    }
-};
+Dispatcher::Dispatcher(BoundedQueue *S_dispatcher_queue, BoundedQueue *N_dispatcher_queue, 
+                       BoundedQueue *W_dispatcher_queue, std::vector<Producer*>* producerVector, int numOfProducers)
+    : S_dispatcher_queue(S_dispatcher_queue), N_dispatcher_queue(N_dispatcher_queue), 
+      W_dispatcher_queue(W_dispatcher_queue), producerVector(producerVector), numOfProducers(numOfProducers) { }
 
+
+Dispatcher::
